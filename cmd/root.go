079b7796a091d80955b8bc9607ee90e7fd922c89
rayyyy <nraavy@gmail.com>
2023-10-09 18:12:37 +0900

    Remove ruby tags from epub files

--- cmd/root.go
+++ cmd/root.go
@@ -22,8 +22,14 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"log"
+	"archive/zip"
+	"epub-ruby-remover/internal"
+	"fmt"
+	"io"
+	"log/slog"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -32,14 +38,109 @@ var rootCmd = &cobra.Command{
 	Use:   "epub-ruby-remover [epub_file]",
 	Short: "Removes ruby tags from epub files",
 	Long:  `Removes ruby tags from epub files.`,
-	Args:  cobra.ExactArgs(1),
+	Args: func(cmd *cobra.Command, args []string) error {
+		if err := cobra.ExactArgs(1)(cmd, args); err != nil {
+			return err
+		}
+
+		// 拡張子を取得してepubかどうかを判定する
+		if match, _ := regexp.MatchString(`\.epub$`, args[0]); !match {
+			return fmt.Errorf("invalid file extension")
+		}
+
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println(args)
+		// read epub file
+		r, err := zip.OpenReader(args[0])
+		if err != nil {
+			slog.Error("Error opening EPUB file:", err)
+			return
+		}
+		defer r.Close()
+
+		// create new epub file
+		filenameWithoutExt := strings.TrimSuffix(args[0], ".epub")
+		newZipFile, err := os.Create(filenameWithoutExt + "_no_ruby.epub")
+		if err != nil {
+			slog.Error("Error creating new EPUB file:", err)
+			return
+		}
+		defer newZipFile.Close()
+
+		newZipWriter := zip.NewWriter(newZipFile)
+		defer newZipWriter.Close()
+
+		slog.Info("Processing EPUB file:", args[0])
+		slog.Info("Files: ", len(r.File))
+
+		re, err := regexp.Compile(`\.(xhtml|html)$`)
+		if err != nil {
+			slog.Error("Error compiling regexp:", err)
+			return
+		}
+
+		for i, f := range r.File {
+			if match := re.MatchString(f.Name); match {
+				slog.Info(fmt.Sprintf("%d / %d - %s", i+1, len(r.File), f.Name))
+
+				rc, err := f.Open()
+				if err != nil {
+					slog.Error("Error opening file:", err)
+					return
+				}
+
+				data, err := io.ReadAll(rc)
+				rc.Close()
+				if err != nil {
+					slog.Error("Error reading file:", err)
+					return
+				}
+
+				modifiedData := internal.RemoveRuby(string(data))
+
+				// 新しいZIPファイルに書き込む
+				newFile, err := newZipWriter.Create(f.Name)
+				if err != nil {
+					slog.Error("Error creating new file:", err)
+					return
+				}
+				_, err = newFile.Write([]byte(modifiedData))
+				if err != nil {
+					slog.Error("Error writing file:", err)
+					return
+				}
+			} else {
+				// XHTML/HTML以外のファイルはそのままコピー
+				newFile, err := newZipWriter.Create(f.Name)
+				if err != nil {
+					slog.Error("Error creating new file:", err)
+					return
+				}
+				oldFile, err := f.Open()
+				if err != nil {
+					slog.Error("Error opening file:", err)
+					return
+				}
+				_, err = io.Copy(newFile, oldFile)
+				oldFile.Close()
+				if err != nil {
+					slog.Error("Error copying file:", err)
+					return
+				}
+			}
+		}
+
+		// done
+		err = newZipWriter.Close()
+		if err != nil {
+			slog.Error("Error closing new EPUB file:", err)
+			return
+		}
+		slog.Info("Done! New EPUB file:", filenameWithoutExt+"_no_ruby.epub")
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -47,13 +148,4 @@ func Execute() {
 	}
 }
 
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.epub-ruby-remover.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-}
+func init() {}
